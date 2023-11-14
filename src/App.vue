6b18369bab6ff395649916c0d530491dd93172ba
Wallacewss2033 <wallace-2033@hotmail.com>
2023-11-13 21:53:16 -0300

    add: app main

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import "../src/services/Socket";
+export default defineComponent({
+  name: "App",
+
+  setup() {
+    const $q = useQuasar();
+
+    // calling here; equivalent to when component is created
+    $q.dark.set(false);
+  },
+});
+</script>
